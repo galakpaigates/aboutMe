4cee711f247b27c1750dae2a52c3db122cfddf5c
galakpaigates <jeedoarkoi2006@gmail.com>
2023-09-28 20:45:32 +0000

    Increased Prank Timeout

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     const notice = document.getElementById("notice");
     const noticeH1 = document.getElementById("noticeH1");
-    const timeout = 750;
+    const timeout = 1000;
     
     setTimeout(() => {
         notice.innerHTML = `<h1>Lol!</h1>`;
-    }, timeout);
+    }, timeout+timeout+timeout);
 
     setTimeout(() => {
         notice.remove();
-    }, timeout+timeout);
+    }, timeout+timeout+timeout+timeout);
 });
